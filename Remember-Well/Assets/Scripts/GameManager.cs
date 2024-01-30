b261762303c412b83df46a9c31ee9a766c495fc1
Ahmad Fadhil <herrahmadfadhil@gmail.com>
2024-01-30 13:42:11 +0100

    Implemented hint feature

--- Remember-Well/Assets/Scripts/GameManager.cs
+++ Remember-Well/Assets/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject audioController;
     public GameObject endGamePanel;
+    public GameObject hintButton;
     private int matchedPairsCount = 0;
     private int totalPairs;
     private List<CardManager> selectedCards = new List<CardManager>();
@@ -33,6 +34,7 @@ public class GameManager : MonoBehaviour
         totalPairs = (gridHeight * gridWidth / 2);
         List<Sprite> shuffledSprites = ShuffleAndPairSprites(cardFrontSprites, gridWidth, gridHeight);
         CreateGrid(gridWidth, gridHeight, shuffledSprites);
+        hintButton.SetActive(true);
     }
 
 
@@ -179,10 +181,71 @@ public class GameManager : MonoBehaviour
 
     }
 
+    public void ShowHint()
+    {
+        List<CardManager> unrevealedCards = FindUnrevealedPairs();
+        if (unrevealedCards.Count >= 2)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, unrevealedCards.Count / 2) * 2;
+            CardManager card1 = unrevealedCards[randomIndex];
+            CardManager card2 = unrevealedCards[randomIndex + 1];
+
+            // Highlight cards
+            card1.HighlightCard();
+            card2.HighlightCard();
+
+            // Schedule unhighlighting the cards
+            StartCoroutine(UnhighlightCardsAfterDelay(card1, card2));
+        }
+    }
+
+    private IEnumerator UnhighlightCardsAfterDelay(CardManager card1, CardManager card2)
+    {
+        yield return new WaitForSeconds(2); // 2 seconds delay
+        card1.UnhighlightCard();
+        card2.UnhighlightCard();
+    }
+
+    private List<CardManager> FindUnrevealedPairs()
+    {
+        List<CardManager> activeCards = new List<CardManager>();
+        CardManager firstMatch = null;
+
+        // First, add all active cards to a list
+        foreach (Transform child in cardParent.transform)
+        {
+            if (child.gameObject.activeSelf) // Check if the card is active
+            {
+                CardManager card = child.GetComponent<CardManager>();
+                if (card != null && !card.IsFlipped()) // Assuming IsFlipped() indicates if the card is face up
+                {
+                    activeCards.Add(card);
+                }
+            }
+        }
+
+        // Next, find a pair of cards with the same front sprite
+        foreach (CardManager card in activeCards)
+        {
+            if (firstMatch == null)
+            {
+                firstMatch = card;
+            }
+            else if (card.frontSprite == firstMatch.frontSprite)
+            {
+                return new List<CardManager> { firstMatch, card };
+            }
+        }
+
+        return new List<CardManager>(); // Return an empty list if no pairs are found
+    }
+
+
     private void EndGame()
     {
         if (endGamePanel != null)
         {
+            hintButton.SetActive(false);
             endGamePanel.SetActive(true);
         }
     }
